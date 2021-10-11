de71442b12b1a445ae9c895b9aab67ad195ec314
coffeeDev <coffee.dev98@gmail.com>
2021-10-11 09:36:43 +0530

    created token server

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const { RtcTokenBuilder, RtcRole } = require("agora-access-token");
+require("dotenv").config();
+const PORT = 8080;
+
+const APP_ID = process.env.APP_ID;
+const APP_CERTIFICATE = process.env.APP_CERTIFICATE;
+
+const app = express();
+
+const nocache = (req, res, next) => {
+  res.header("Cache-Content", "private, no-cache, no-store, must-revalidate ");
+  res.header("Expires", "-1");
+  res.header("Pragma", "no-cache");
+  next();
+};
+
+const generateAccessToken = (req, res) => {
+  // set response header
+  res.header("Access-Control-Allow-Origin", "*");
+  // get channel name
+  const channel = req.query.channel;
+  if (!channel) {
+    return res.status(500).json({ error: "channel is required" });
+  }
+  // get uid
+  let uid = req.query.uid;
+  if (!uid || uid === "") {
+    uid = 0;
+  }
+  // get role
+  let role = RtcRole.SUBSCRIBER;
+  if (req.query.role === "publisher") {
+    role = RtcRole.PUBLISHER;
+  }
+  // get the expiration time
+  let expireTime = req.query.expireTime;
+  if (!expireTime || expireTime === "") {
+    expireTime = 3600;
+  } else {
+    expireTime = parseInt(expireTime, 10);
+  }
+  // calculate priviledge expire time
+  const currentTime = Math.floor(Date.now() / 1000);
+  const priviledgeExpireTime = currentTime + expireTime;
+  // build the token
+  const token = RtcTokenBuilder.buildTokenWithUid(
+    APP_ID,
+    APP_CERTIFICATE,
+    channel,
+    uid,
+    role,
+    priviledgeExpireTime
+  );
+  // return the token
+  return res.json({ token: token });
+};
+
+app.get("/access_token", nocache, generateAccessToken);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+  console.log(`APP_ID: ${APP_ID}`);
+  console.log(`APP_CERTIFICATE: ${APP_CERTIFICATE}`);
+});
